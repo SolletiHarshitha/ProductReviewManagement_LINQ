1172517cfacbc0d691e5ca9190073387e5a933c7
Solleti Harshitha <solletiharshitha@gmail.com>
2021-08-05 23:54:12 +0530

    [Harshitha] Creating Product List

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------Product Review Management--------------");
+            List<ProductReview> productReviewList = new List<ProductReview>();
+            Management.AddProductReviews(productReviewList);
+        }
+    }
+}
